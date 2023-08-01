ddae09ef2add022b4e00924cc47b05ac8dd0c555
eunha <gangssu0@gmail.com>
2023-08-01 18:01:09 +0900

    슬라이드

--- script/script.js
+++ script/script.js
@@ -46,12 +46,34 @@ const pawin_slide = new Swiper('#pawin_slide',{
         // effect:'fade',// 제자리 변경 효과
         // direction:'vertical' // 수직방향
 });
-// const new_slide = new Swiper('#second_slide',{
-//     autoplay:{delay:1000, disableOnInteraction:false}, loop:true,
-//     navigation: {
-//         // next, prev 객체 연결 시 부모를 안 적으면
-//         // body 안에 있는 모든 swiper-next,prev 를 인식하기 때문에
-//         // 개별인식가능한 부모이름을 반드시 앞에 먼저 작성한다.
-//         nextEl: '#second_slide .swiper-button-next',
-//         prevEl: '#second_slide .swiper-button-prev',}
-// });
+const new_slide = new Swiper('#second_slide',{
+    autoplay:{delay:1000, disableOnInteraction:false}, loop:true,
+    navigation: {
+         // next, prev 객체 연결 시 부모를 안 적으면
+         // body 안에 있는 모든 swiper-next,prev 를 인식하기 때문에
+         // 개별인식가능한 부모이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#second_slide .swiper-button-next',
+        prevEl: '#second_slide .swiper-button-prev',}
+});
+// pawin-slide2
+const pawin_slide2 = new Swiper('#pawin_slide2',{
+    autoplay:{delay:1000},
+    loop:true,
+    effect:'fade',
+});
+// 입양정보 slide
+const dog_info_slide = new Swiper('#dog_info_slide',{
+    slidesPerView:'auto', // breakpoints옵션 추가시 'auto' 한번에 보이는 슬라이드 갯수
+    spaceBetween:20, // 슬라이드 사이 여백
+    autoplay:{delay:1000},
+    loop:true,
+    // swiper-slide 반응형웹 옵션
+    breakpoints:{
+        // 해상도:{옵션:값}
+        // 해상도 순서는 작은 해상도 -> 큰 해상도 순으로 작성.
+        400:{slidesPerView:2}, // 400~899
+        // 700:{slidesPerView:2}, // 700이상일 경우 슬라이드 2개 표시
+        900:{slidesPerView:3}, // 900이상일 경우 슬라이드 3개 표시
+        1160:{slidesPerView:4}, // 1160이상일 경우 슬라이드 4개 표시
+    }
+});
